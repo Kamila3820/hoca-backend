d3c6333a5313298d72e103f00e165f49a02e27f2
Kamila3820 <thanadonsaojarkaval@gmail.com>
2024-08-22 16:30:05 +0700

    first part initial project

---
+++ config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"sync"
+	"time"
+)
+
+type (
+	Config struct {
+		Server   *Server   `mapstructure:"server" validate:"required"`
+		State    *State    `mapstructure:"state" validate:"required"`
+		Database *Database `mapstructure:"database" validate:"required"`
+	}
+
+	Server struct {
+		Port         int           `mapstructure:"port" validate:"required"`
+		AllowOrigins []string      `mapstructure:"allowOrigins" validate:"required"`
+		BodyLimit    string        `mapstructure:"bodyLimit" validate:"required"`
+		TimeOut      time.Duration `mapstructure:"timeout" validate:"required"`
+	}
+
+	OAuth2 struct {
+	}
+
+	endpoint struct {
+	}
+
+	State struct {
+		Secret    string        `mapstructure:"secret" validate:"required"`
+		ExpiresAt time.Duration `mapstructure:"expiresAt" validate:"required"`
+		Issuer    string        `mapstructure:"issuer" validate:"required"`
+	}
+
+	Database struct {
+		Host     string `mapstructure:"host" validate:"required"`
+		Port     int    `mapstructure:"port" validate:"required"`
+		User     string `mapstructure:"user" validate:"required"`
+		Password string `mapstructure:"password" validate:"required"`
+		DBName   string `mapstructure:"dbname" validate:"required"`
+		SSLMode  string `mapstructure:"sslmode" validate:"required"`
+		Schema   string `mapstructure:"schema" validate:"required"`
+	}
+)
+
+var (
+	once           sync.Once
+	configInstance *Config
+)
+
+func ConfigGetting() *Config {
+	once.Do(func() {
+
+	})
+	return nil
+}
