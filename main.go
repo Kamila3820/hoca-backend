a4b218e4c512c9c95e0f8ba2a2ecd6f7a56ae804
Kamila3820 <thanadonsaojarkaval@gmail.com>
2024-08-27 14:10:36 +0700

    - initial echo server && health check

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
-package mai
+package main
+
+import (
+	"fmt"
+
+	"github.com/Kamila3820/hoca-backend/config"
+	"github.com/Kamila3820/hoca-backend/databases"
+	"github.com/Kamila3820/hoca-backend/server"
+)
 
 func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
 
+	fmt.Println("Hello, World!")
+	server.Start()
 }
